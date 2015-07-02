3d272eb4f7592009cf027a223bdaa454a49469c3
Mustafa Altun <gmail@mustafaaltun.com>
2015-07-02 17:59:23 +0300

    Add client-side validation to Message struct

--- message.go
+++ message.go
@@ -1,9 +1,23 @@
 package gcm
 
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+const (
+	maxRegistrationIDs = 1000
+	maxTTL             = 4 * 7 * 24 * 60 * 60
+	maxPriority        = 10
+)
+
+type RegistrationID string
+
 type Message struct {
 	// Targets
-	To              string   `json:"to,omitempty"`
-	RegistrationIDs []string `json:"registration_ids"`
+	To              RegistrationID   `json:"to,omitempty"`
+	RegistrationIDs []RegistrationID `json:"registration_ids"`
 
 	// Options
 	CollapseKey           string `json:"collapse_key,omitempty"`
@@ -32,4 +46,57 @@ type Notification struct {
 	BodyLocArgs  string `json:"body_loc_args,omitempty"`
 	TitleLocArgs string `json:"title_loc_args,omitempty"`
 	TitleLocKey  string `json:"title_loc_key,omitempty"`
+}
+
+var reservedDataKeys = []string{"from", "collapse_key"}
+
+var (
+	errNoRegID               = errors.New("requires at least one registration id")
+	errBothToAndRegID        = errors.New("cannot set both 'to' and 'registration_ids' field")
+	errInvalidPriority       = errors.New("priority value must be between 0 - 10")
+	errExceedMaxRegIDs       = fmt.Errorf("registration_ids field cannot contain more than %d registration id", maxRegistrationIDs)
+	errInvalidTTL            = fmt.Errorf("time_to_live value must be between 0 - %d", maxTTL)
+	errReservedDataKey       = fmt.Errorf("data must not contain a reserved key. Reserved keys: %s", strings.Join(reservedDataKeys, ", "))
+	errReservedDataKeyPrefix = errors.New("data must not contain keys beginning with gcm or google")
+)
+
+func (m *Message) Validate() error {
+	if (m.To == "") && (len(m.RegistrationIDs) == 0) {
+		return errNoRegID
+	}
+
+	if (m.To != "") && (len(m.RegistrationIDs) > 0) {
+		return errBothToAndRegID
+	}
+
+	// check len(RegistrationIDs) > 1000
+	if len(m.RegistrationIDs) > maxRegistrationIDs {
+		return errExceedMaxRegIDs
+	}
+
+	// check TTL
+	if m.TTL > maxTTL {
+		return errInvalidTTL
+	}
+
+	// check priority
+	if m.Priority > maxPriority {
+		return errInvalidPriority
+	}
+
+	// check data keys...
+	for key := range m.Data {
+		for _, rkey := range reservedDataKeys {
+			if key == rkey {
+				return errReservedDataKey
+			}
+		}
+
+		if strings.HasPrefix(key, "google") || strings.HasPrefix(key, "gcm") {
+			return errReservedDataKeyPrefix
+		}
+	}
+	//
+
+	return nil
 }
