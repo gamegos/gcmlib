7a0509274dc79600eca57ce8d3fbc4743da87acc
Mustafa Altun <gmail@mustafaaltun.com>
2015-07-09 17:18:31 +0300

    Add unit tests for message

---
+++ message_test.go
@@ -0,0 +1,30 @@
+package gcm
+
+import (
+	"reflect"
+	"testing"
+)
+
+var messageValidateTests = []struct {
+	msg *Message
+	err error
+}{
+	{&Message{}, errNoRegID},
+	{&Message{To: "foo", RegistrationIDs: []RegistrationID{"id0"}}, errBothToAndRegID},
+	{&Message{RegistrationIDs: make([]RegistrationID, 1001)}, errExceedMaxRegIDs},
+	{&Message{To: "foo", TTL: maxTTL + 1}, errInvalidTTL},
+	{&Message{To: "foo", Priority: maxPriority + 1}, errInvalidPriority},
+	{&Message{To: "foo", Data: map[string]string{"from": "bar"}}, errReservedDataKey},
+	{&Message{To: "foo", Data: map[string]string{"google.key": "bar"}}, errReservedDataKeyPrefix},
+
+	{&Message{To: "foo", Data: map[string]string{"foo": "bar"}}, nil},
+}
+
+func TestValidate(t *testing.T) {
+	for _, tt := range messageValidateTests {
+		have, want := tt.msg.Validate(), tt.err
+		if !reflect.DeepEqual(have, want) {
+			t.Errorf("Message.Validate(%q): have: %#v, want: %#v\n", tt.msg, have, want)
+		}
+	}
+}
